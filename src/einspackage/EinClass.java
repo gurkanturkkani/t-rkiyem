584184a5a59638471698d84b27e09c9b49379d5f
gurkanturkkani <67173925+gurkanturkkani@users.noreply.github.com>
2020-06-22 22:27:41 +0200

    Update EinClass.java
    ilk commit from remote repository

--- src/einspackage/EinClass.java
+++ src/einspackage/EinClass.java
@@ -5,7 +5,9 @@ public class EinClass {
 	public static void main(String[] args) {
 		System.out.println("Eins commit from local repository");
 		System.out.println("Zweite commit from local repository");
-		System.out.println(" commit from local repository");	
+		System.out.println(" commit from local repository");
+		System.out.println(" Eins commit from remote repository");
+		
 	}
 
 }
