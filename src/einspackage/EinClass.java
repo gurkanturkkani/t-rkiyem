210bbc2f4aec9d74f16aaaef1a5b6b2926204c9b
gurkan <grkn1978.t@gmail.com>
2020-06-22 14:58:52 +0200

    ikinci commit local repositoryden

--- src/einspackage/EinClass.java
+++ src/einspackage/EinClass.java
@@ -4,7 +4,7 @@ public class EinClass {
 
 	public static void main(String[] args) {
 		System.out.println("Eins commit from local repository");
-		
+		System.out.println("Zweite commit from local repository");
 
 	}
 
