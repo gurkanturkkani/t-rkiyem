9cfd6e1e69278f50bc1e735e8d5c06eccc67e74f
gurkan <grkn1978.t@gmail.com>
2020-06-22 23:30:10 +0200

    son conflickt

--- src/einspackage/EinClass.java
+++ src/einspackage/EinClass.java
@@ -7,7 +7,7 @@ public class EinClass {
 		System.out.println("Zweite commit from local repository");
 
 		
-		System.out.println("commit yapmak istiyorum ");
+		System.out.println("ikinci kez conflict yapiyoruz ");
 
 		System.out.println(" Eins commit from remote repository");
 		
